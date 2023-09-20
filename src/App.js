ae1a2ae34a2257133c82b21028b1fb8ef80dde9d
GiorgosDrivas <drivasgiorgos9@gmail.com>
2023-09-21 00:23:09 +0300

    Tailwind css

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
 
     function Random() {
         const randomCharsArray = [];
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 3; i++) { // Fill the array 
             randomCharsArray.push(upper[Math.floor(Math.random() * upper.length)]);
             randomCharsArray.push(lower[Math.floor(Math.random() * lower.length)]);
             randomCharsArray.push(num[Math.floor(Math.random() * num.length)]);
@@ -29,29 +29,30 @@ export default function App() {
         document.body.removeChild(textArea);
     }
 
+
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="wrap d-flex flex-column justify-content-center align-items-center">
-                        <div className='position-relative mb-3'>
-                            {randomChars ? (
-                                <p>{randomChars}</p>
-                            ) : (
-                                <p>Click the button</p>
-                            )}
-                            {randomChars && (
-                                <a title="Copy to clipboard" className='position-absolute copy' onClick={copyToClipboard}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-clipboard" viewBox="0 0 16 16">
-                                        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
-                                        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
-                                    </svg>
-                                </a>
-                            )}
-                        </div>
-                        <button className='generate' onClick={Random}>Generate</button>
-                    </div>
+        <div className="container mx-auto">
+            <div className="flex flex-col justify-center items-center h-screen">
+                <div className='relative mb-3'>
+                    {randomChars ? (
+                        <>
+                            <p>{randomChars}</p>
+                        </>
+                    ) : (
+                        <>
+                            <p>Click the button</p>
+                        </>
+                    )}
+                    {randomChars && (
+                        <a title="Copy to clipboard" className='absolute copy' onClick={copyToClipboard}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="black" className="bi bi-clipboard" viewBox="0 0 16 16">
+                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+                            </svg>
+                        </a>
+                    )}
                 </div>
+                <button className='generate' onClick={Random}>Generate</button>
             </div>
         </div>
     );
